74d595982cfa9ca68e4484086aabe3be25aaf8f1
vijeyasri16 <130274457+vijeyasri16@users.noreply.github.com>
2023-08-06 21:36:38 +0530

    first_commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,9 @@ public // Your First Program
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        System.out.println("Hello, Vijeyasri!"); 
+        System.out.println("Hello, Vijeyasri how are you !"); 
+
+
     }
 } 
